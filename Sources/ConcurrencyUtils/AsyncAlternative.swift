5b5aa014cd736a4572e466dcab351a3fc5f133f9
floormatgen <unwound.podium_0y@icloud.com>
2026-04-20 02:21:40 +1000

    Added comment and availability to AsyncAlternative

--- Sources/ConcurrencyUtils/AsyncAlternative.swift
+++ Sources/ConcurrencyUtils/AsyncAlternative.swift
@@ -1,8 +1,9 @@
+
+// MARK: - Macro
+
+/// Creates an async mirror of a provided function
+/// 
+/// This is useful for "with-style" methods that take a body closure.
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, visionOS 1.0, *)
 @attached(peer, names: overloaded)
 public macro AsyncAlternative() = #externalMacro(module: "ConcurrencyUtilsMacros", type: "AsyncAlternative")
-
-
-@AsyncAlternative @Sendable nonisolated
-func foo(_ operation: () -> Void) -> Void {
-  return operation()
-}
